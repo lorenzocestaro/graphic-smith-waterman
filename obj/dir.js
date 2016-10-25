841e26f8efa025f8862da9dca5dce2593c8c3067
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-10-25 15:05:56 +0200

    obj/dir.js rewrite, remove old tests

--- obj/dir.js
+++ obj/dir.js
@@ -1,8 +1,10 @@
-var directions = {
-	none: 0,
-  diag: 1,
-  left: 2,
-  up: 3
+'use strict';
+
+const directions = {
+    none: 0,
+    diag: 1,
+    left: 2,
+    up: 3
 };
 
 module.exports = directions;
