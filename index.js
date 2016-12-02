b916ea78d323570699f4c7860127662c726f1eda
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-12-02 09:57:26 +0100

    Remove console log from index.js

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const align = require('./alignment').align;
 const compute = require('./algorithm');
 const score = require('./alignment').score;
 
-console.log(align(
-    'thisisanexample',
-    'thisisoneexample',
-    (k) => -Math.pow(k, 2),
-    (a, b) => (a === b) ? 2 : -1
-));
-
 module.exports = {
     align: align,
     compute: compute,
