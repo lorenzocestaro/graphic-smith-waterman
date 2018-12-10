79f3fddd04c15d40b721e0aa226a95e4104e8517
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2018-12-10 23:42:37 +0100

    Add matrix iterator and shape utilities

--- src/v2/matrix.utils.js
+++ src/v2/matrix.utils.js
@@ -1,16 +1,33 @@
-const createMatrix = ({ width, heigth }) => Array(heigth)
-    .fill(0)
-    .map(() => Array(width).fill(0));
+const createMatrix = ({ width, heigth }) =>
+    Array(heigth)
+        .fill(0)
+        .map(() => Array(width).fill(0));
 
 const extractRow = ({ matrix, row, col }) => matrix[row].slice(0, col + 1);
 
-const extractColumn = ({ matrix, row, col }) => matrix
-    .slice(0, row + 1)
-    .map(_row => _row.slice(col, col + 1))
-    .reduce((prev, curr) => [...prev, ...curr], []);
+const extractColumn = ({ matrix, row, col }) =>
+    matrix
+        .slice(0, row + 1)
+        .map(_row => _row.slice(col, col + 1))
+        .reduce((prev, curr) => [...prev, ...curr], []);
+
+function* matrixIterator(matrix) {
+    const width = matrix[0].length;
+    const heigth = matrix.length;
+
+    for (let row = 0; row < heigth; row += 1) {
+        for (let col = 0; col < width; col += 1) {
+            yield matrix[row][col];
+        }
+    }
+}
+
+const shape = matrix => ({ width: (matrix[0] && matrix[0].length) || 0, heigth: matrix.length });
 
 module.exports = {
     createMatrix,
     extractColumn,
     extractRow,
-}
+    matrixIterator,
+    shape,
+};
--- src/v2/matrix.utils.spec.js
+++ src/v2/matrix.utils.spec.js
@@ -1,47 +1,50 @@
-const { createMatrix, extractColumn, extractRow } = require('./matrix.utils');
+const {
+    createMatrix,
+    extractColumn,
+    extractRow,
+    matrixIterator,
+    shape,
+} = require('./matrix.utils');
 
+// prettier-ignore
 const matrixFixture = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [1, 2, 3, 4],
-    [5, 6, 7, 8],
+    [5, 6, 7, 8]
 ];
 
 describe('Matrix creation', () => {
     it.each([
-        [{ width: 10, heigth: 3 }, [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]],
-        [{ width: 3, heigth: 10 }, [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-        ]],
-        [{ width: 3, heigth: 3 }, [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-        ]],
-        [{ width: 2, heigth: 2 }, [
-            [0, 0],
-            [0, 0],
-        ]],
-        [{ width: 1, heigth: 1 }, [
-            [0],
-        ]],
+        [
+            { width: 10, heigth: 3 },
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+        ],
+        [
+            { width: 3, heigth: 10 },
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ],
+        ],
+        [{ width: 3, heigth: 3 }, [[0, 0, 0], [0, 0, 0], [0, 0, 0]]],
+        [{ width: 2, heigth: 2 }, [[0, 0], [0, 0]]],
+        [{ width: 1, heigth: 1 }, [[0]]],
         [{ width: 0, heigth: 0 }, []],
-    ])(
-        'should return a matrix with the specified dimensions',
-        (input, output) => expect(createMatrix(input)).toEqual(output)
+    ])('should return a matrix with the specified dimensions', (input, output) =>
+        expect(createMatrix(input)).toEqual(output),
     );
 });
 
@@ -81,4 +84,55 @@ describe('Matrix row extraction', () => {
         });
         expect(extractedColumn).toEqual([1, 2, 3, 4]);
     });
+});
+
+describe('Matrix iterator', () => {
+    it('should return a generator function', () => {
+        const generator = matrixIterator(matrixFixture);
+        expect(generator.next().value).toBe(1);
+    });
+
+    it('should work as an iterable', () => {
+        const mock = jest.fn();
+        const generator = matrixIterator(matrixFixture);
+        for (const item of generator) {
+            mock(item);
+        }
+        expect(mock).toHaveBeenCalledTimes(16);
+    });
+});
+
+describe('Shape', () => {
+    it.each([
+        [
+            { width: 10, heigth: 3 },
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+        ],
+        [
+            { width: 3, heigth: 10 },
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ],
+        ],
+        [{ width: 3, heigth: 3 }, [[0, 0, 0], [0, 0, 0], [0, 0, 0]]],
+        [{ width: 2, heigth: 2 }, [[0, 0], [0, 0]]],
+        [{ width: 1, heigth: 1 }, [[0]]],
+        [{ width: 1, heigth: 1 }, [['asd']]],
+        [{ width: 0, heigth: 0 }, []],
+    ])('should return the matrix shape', (expectedShape, matrix) =>
+        expect(shape(matrix)).toEqual(expectedShape),
+    );
 });
