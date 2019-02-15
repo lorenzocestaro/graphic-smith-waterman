4d0e21834038f353abdda5088791c05725cca7dc
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2019-02-15 16:35:18 +0100

    Add tests for utils.js

--- src/utils.js
+++ src/utils.js
@@ -8,11 +8,21 @@ const relu = x => Math.max(x, 0);
 
 const reverse = x => -x;
 
+const nanException = () => {
+    throw TypeError('Non number input to decreaseAndRectify().');
+};
+
+const throwIfNotNumber = x => (Number.isNaN(Number(x)) ? nanException() : x);
+
 module.exports = {
     apply,
     decreaseAndRectify: pipe(
+        throwIfNotNumber,
         decreaseByOne,
         relu,
     ),
-    reverse,
+    reverse: pipe(
+        throwIfNotNumber,
+        reverse,
+    ),
 };
---
+++ src/utils.spec.js
@@ -0,0 +1,50 @@
+const { apply, decreaseAndRectify, reverse } = require('./utils');
+
+describe('Apply', () => {
+    it('should return a function', () => {
+        const applied = apply(jest.fn());
+        expect(applied).toBeInstanceOf(Function);
+    });
+});
+
+describe('Applied function', () => {
+    it('should be called as many times as the items in the iterator', () => {
+        const array = [1, 2, 3, 4, 5];
+        const fnToApply = jest.fn();
+        const applyFn = apply(fnToApply);
+        applyFn(array);
+        expect(fnToApply).toBeCalledTimes(array.length);
+    });
+    it('should raise an error when the input argument is not an array', () => {
+        const faultyInput = 'string';
+        const fnToApply = jest.fn();
+        const applyFn = apply(fnToApply);
+        expect(() => applyFn(faultyInput)).toThrowError(TypeError);
+    });
+});
+
+describe('Decrease and rectify', () => {
+    it.each([1, 2, 3, 4, 5, 10, 20, 100, 1.345])(
+        'should decrement an integer greater then zero by one',
+        n => expect(decreaseAndRectify(n)).toEqual(n - 1),
+    );
+    it.each([0, -1, -2, -3, -54, -156, -89689, 0.34])(
+        'should return zero if decrementing the input would return a negative number',
+        n => expect(decreaseAndRectify(n)).toEqual(0),
+    );
+    it.each(['string', [1, 2, 3], { a: 1, b: 2 }])(
+        'should raise TypeError for non number inputs',
+        n => expect(() => decreaseAndRectify(n)).toThrowError(TypeError),
+    );
+});
+
+describe('Reverse', () => {
+    it.each([0, -1, 2, -3, 54, -156, -89689, 0.34])(
+        'should reverse the sign of every numeric input',
+        n => expect(reverse(n)).toEqual(-1 * n),
+    );
+    it.each(['string', [1, 2, 3], { a: 1, b: 2 }])(
+        'should raise TypeError for non number inputs',
+        n => expect(() => reverse(n)).toThrowError(TypeError),
+    );
+});
